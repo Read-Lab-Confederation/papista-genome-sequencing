935ccfe3e97f187bc4e9450837f459c404220085
Robert A Petit III <robert.petit@emory.edu>
2014-07-10 10:02:20 -0700

    Added another sequening run

--- commands.sh
+++ commands.sh
@@ -2,4 +2,5 @@
 poretools fastq --min-length 500 fast5/run_001/ | sed 's/:fast5.*$//' | gzip --best - > reads/run_001.fastq.gz
 poretools fastq --min-length 500 fast5/run_002/ | sed 's/:fast5.*$//' | gzip --best - > reads/run_002.fastq.gz
 poretools fastq --min-length 500 fast5/run_003/ | sed 's/:fast5.*$//' | gzip --best - > reads/run_003.fastq.gz
+poretools fastq --min-length 500 fast5/run_004/ | sed 's/:fast5.*$//' | gzip --best - > reads/run_004.fastq.gz
 
